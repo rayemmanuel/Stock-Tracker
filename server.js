26c0d0c655002aca37fdfa14d281b4c7763a638d
mrechy-ai <mrechy@tip.edu.ph>
2025-10-16 21:10:53 +0800

    sana gumana

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const path = require('path');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
